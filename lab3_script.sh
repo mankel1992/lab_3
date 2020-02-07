af4759fda1b9d2769c53bbfad0195032d77b25c8
mankel1992 <mankel1992@gmail.com>
2020-02-07 16:29:22 -0700

    submitting everything

---
+++ lab3_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Authors : aaron mankel solo
+# Date: 2/7/2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+echo "Enter a filename: "
+read file1
+echo "enter a regex:"
+read type
+grep -c $type $file1
+
+echo "enter a regex:"
+read type2
+grep -o $type2 $file1
+
+echo "enter a regex:"
+read type3
+grep -c $type3 $file1 >> email_results.txt 
